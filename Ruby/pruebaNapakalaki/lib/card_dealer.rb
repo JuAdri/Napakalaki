32536f595e3e4493a563003cf1b592096a7ad25a
Adrian Peña <adrian.j.p.r@gmail.com>
2016-11-10 13:12:26 +0100

    Tesoros creados

--- Ruby/pruebaNapakalaki/lib/card_dealer.rb
+++ Ruby/pruebaNapakalaki/lib/card_dealer.rb
@@ -8,7 +8,37 @@ class CardDealer
   attr_reader :instance
   
   def self.initTreasureCardDeck
-    
+    @unsuedTreasures << Treasure.new("¡Si mi amo!", 4, TreasureKind::HELMET)
+    @unusedTreasures << Treasure.new("Botas de investigacion", 3, TreasureKind::SHOES)
+    @unusedTreasures << Treasure.new("Capucha de Cthulhu", 3, TreasureKind::HELMET)
+    @unusedTreasures << Treasure.new("A prueba de babas", 2, TreasureKind::ARMOR)
+    @unusedTreasures << Treasure.new("Botas de lluvia acida", 1, TreasureKind::BOTHHANDS)
+    @unusedTreasures << Treasure.new("Casco minero", 2, TreasureKind::HELMET)
+    @unusedTreasures << Treasure.new("Ametralladora ACME", 4, TreasureKind::BOTHHANDS)
+    @unusedTreasures << Treasure.new("Camiseta de la ETSIIT", 1, TreasureKind::ARMOR)
+    @unusedTreasures << Treasure.new("Clavo de rail ferroviario", 3, TreasureKind::ONEHAND)
+    @unusedTreasures << Treasure.new("Cuchillo de sushi arcano", 2, TreasureKind::ONEHAND)
+    @unusedTreasures << Treasure.new("Fez alopodo", 3, TreasureKind::HELMET)
+    @unusedTreasures << Treasure.new("Hacha prehistorica", 2, TreasureKind::ONEHAND)
+    @unusedTreasures << Treasure.new("El aparato del PR. Tesla", 4, TreasureKind::ARMOR)
+    @unusedTreasures << Treasure.new("Gaita", 4, TreasureKind::BOTHHANDS)
+    @unusedTreasures << Treasure.new("Insecticida", 2, TreasureKind::ONEHAND)
+    @unusedTreasures << Treasure.new("Escopeta de 3 cañones", 4, TreasureKind::BOTHHANDS)
+    @unusedTreasures << Treasure.new("Garabato mistico", 2, TreasureKind::ONEHAND)
+    @unusedTreasures << Treasure.new("La rebeca metalica", 2, TreasureKind::ARMOR)
+    @unusedTreasures << Treasure.new("Lanzallamas", 4, TreasureKind::BOTHHANDS)
+    @unusedTreasures << Treasure.new("Necro-comicón", 1, TreasureKind::ONEHAND)
+    @unusedTreasures << Treasure.new("Necronomicón", 5, TreasureKind::BOTHHANDS)
+    @unusedTreasures << Treasure.new("Linterna a 2 manos", 3, TreasureKind::BOTHHANDS)
+    @unusedTreasures << Treasure.new("Necro-gnomicón", 2, TreasureKind::ONEHAND)
+    @unusedTreasures << Treasure.new("Necrotelecom", 2, TreasureKind::HELMET)
+    @unusedTreasures << Treasure.new("Mazo de los antiguos", 3, TreasureKind::ONEHAND)
+    @unusedTreasures << Treasure.new("Necro-playboycón", 3, TreasureKind::ONEHAND)
+    @unusedTreasures << Treasure.new("Porra preternatural", 2, TreasureKind::ONEHAND)
+    @unusedTreasures << Treasure.new("Shogulador", 1, TreasureKind::BOTHHANDS)
+    @unusedTreasures << Treasure.new("Varita de atizamiento", 3, TreasureKind::ONEHAND)
+    @unusedTreasures << Treasure.new("Tentáculo de pega", 2, TreasureKind::HELMET)
+    @unusedTreasures << Treasure.new("Zapato deja-amigos", 1, TreasureKind::SHOES)
   end
   
   def self.initMonsterCardDeck
