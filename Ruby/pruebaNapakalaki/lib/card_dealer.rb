966a530e7dc340df1773ef4a7f1d75bd69d7145c
Juane Garcia <juane_619@hotmail.com>
2016-11-10 13:01:15 +0100

    monstruos corregidos

--- Ruby/pruebaNapakalaki/lib/card_dealer.rb
+++ Ruby/pruebaNapakalaki/lib/card_dealer.rb
@@ -107,50 +107,7 @@ class CardDealer
     bad_consequence = BadConsequence.newLevelSpecificTreasures('Te faltan manos para tanta cabeza. Pierdes 3 niveles y tus tesoros visibles de las manos.', 3, [TreasureKind::ONEHAND], [TreasureKind::BOTHHANDS])
     @unusedMonsters << Monster.new('Bicefalo', 21, prize, bad_consequence)
     
-    # Monstruo 10 -> Los hondos
-    prize = Prize.new(2, 1)
-    badConsequence = BadConsequence.newDeath('Estos monstruos resultan bastante superficiales y te aburren mortalmente. Estas muerto')
-    @unusedMonsters << Monster.new('Los hondos', 8, prize, badConsequence)
-
-    # Monstruo 11 -> Semillas Cthulhu
-    prize = Prize.new(2, 1)
-    badConsequence = BadConsequence.newLevelNumberOfTreasures('Pierdes 2 niveles y 2 tesoros ocultos.', 2, 0, 2)
-    @unusedMonsters << Monster.new('Semillas Cthulhu', 4, prize, badConsequence)
-
-    # Monstruo 12 -> Dameargo
-    prize = Prize.new(2, 1)
-    badConsequence = BadConsequence.newLevelSpecificTreasures('Te intentas escaquear. Pierdes una mano visible.', 0, [TreasureKind::ONEHAND], Array.new)
-    @unusedMonsters << Monster.new('Dameargo', 1, prize, badConsequence)
-
-    # Monstruo 13 -> Pollipólipo volante
-    prize = Prize.new(2, 1)
-    badConsequence = BadConsequence.newLevelNumberOfTreasures('Da mucho asquito. Pierdes 3 niveles.', 3, 0, 0)
-    @unusedMonsters << Monster.new('Pollipólipo volante', 3, prize, badConsequence)
-
-    # Monstruo 14 -> Yskhtihyssg-Goth
-    prize = Prize.new(3, 1)
-    badConsequence = BadConsequence.newDeath('No le hace gracia que pronuncien mal su nombre. Estas muerto')
-    @unusedMonsters << Monster.new('Yskhtihyssg-Goth', 14, prize, badConsequence)
-
-    # Monstruo 15 -> Familia feliz
-    prize = Prize.new(3, 1)
-    badConsequence = BadConsequence.newDeath('La familia te atrapa. Estás muerto.')
-    @unusedMonsters << Monster.new('Familia feliz', 1, prize, badConsequence)
-
-    # Monstruo 16 -> Roboggoth
-    prize = Prize.new(2, 1)
-    badConsequence = BadConsequence.newLevelSpecificTreasures('La quinta directiva primaria te obliga a perder 2 niveles y un tesoro 2 manos visible', 2, [TreasureKind::BOTHHAND], Array.new)
-    @unusedMonsters << Monster.new('Roboggoth', 8, prize, badConsequence)
-
-    # Monstruo 17 -> El espía sordo
-    prize = Prize.new(1, 1)
-    badConsequence = BadConsequence.newLevelSpecificTreasures('Te asusta en la noche. Pierdes un casco visible.', 0, [TreasureKind::HELMET], Array.new)
-    @unusedMonsters << Monster.new('El espía sordo', 5, prize, badConsequence)
-
-    # Monstruo 18 -> Tongue
-    prize = Prize.new(2, 1)
-    badConsequence = BadConsequence.newLevelNumberOfTreasures('Menudo susto te llevas. Pierdes 2 niveles y 5 tesoros visibles.', 2, 5, 0)
-    @unusedMonsters << Monster.new('Tongue', 19, prize, badConsequence)
+    
   end
   
   def self.shuffleTreasures
